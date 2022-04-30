8ec355091177a018a2e6efa102c23a772a672384
cihancalli <cihan.callii@gmail.com>
2022-04-30 13:53:10 +0300

    Classical Method

--- app/src/main/java/com/zerdasoftware/usingmvvm/MainActivity.kt
+++ app/src/main/java/com/zerdasoftware/usingmvvm/MainActivity.kt
@@ -2,10 +2,37 @@ package com.zerdasoftware.usingmvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textView_result.text = "0"
+
+        button_collection.setOnClickListener {
+            val getNumber1 = editText_sayi_1.text.toString()
+            val getNumber2 = editText_sayi_2.text.toString()
+
+            val number1 = getNumber1.toInt()
+            val number2 = getNumber2.toInt()
+
+            val collect = number1 + number2
+
+            textView_result.text = "${collect}"
+        }
+
+        button_multiplication.setOnClickListener {
+            val getNumber1 = editText_sayi_1.text.toString()
+            val getNumber2 = editText_sayi_2.text.toString()
+
+            val number1 = getNumber1.toInt()
+            val number2 = getNumber2.toInt()
+
+            val multiple = number1 * number2
+
+            textView_result.text = "${multiple}"
+        }
     }
 }
